1ea79a26526188eba044f86cea7d16190daa7f68
bhotiadhruv1 <bhotiadhruv1@gmail.com>
2026-07-01 15:30:20 -0400

    july 1st

--- app/admin/courses/[courseId]/edit/_components/CourseStructure.tsx
+++ app/admin/courses/[courseId]/edit/_components/CourseStructure.tsx
@@ -2,12 +2,17 @@
 
 import { AdminGetCourseType } from "@/app/data/admin/admin-get-course";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Collapsible } from "@/components/ui/collapsible";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 import { useSortable } from "@dnd-kit/react/sortable";
-import { GripVertical } from "lucide-react";
+import { ChevronDown, ChevronRight, GripVertical } from "lucide-react";
 import { useRef, useState } from "react";
 import { DragDropProvider } from "@dnd-kit/react";
 import { move } from "@dnd-kit/helpers";
+import { Button } from "@/components/ui/button";
 
 interface Props {
   courseData: AdminGetCourseType;
@@ -25,7 +30,9 @@ interface SortableProps {
   };
 }
 
-function Sortable({ id, index, isOpen, onToggle, title }: SortableProps) {
+function LessonSortable();
+
+function ChapterSortable({ id, index, isOpen, onToggle, title }: SortableProps) {
   const [element, setElement] = useState<HTMLDivElement | null>(null);
   const handleRef = useRef<HTMLButtonElement | null>(null);
   const { isDragging } = useSortable({
@@ -37,22 +44,39 @@ function Sortable({ id, index, isOpen, onToggle, title }: SortableProps) {
 
   return (
     <Card ref={setElement} className="item cursor-pointer p-4">
-      <CardHeader>
-        <CardTitle>
-          <button
-            ref={handleRef}
-            className="cursor-grab opacity-60 hover:opacity-100 flex gap-2 items-center"
-          >
-            <GripVertical className="size-4" /> {title}
-          </button>
-        </CardTitle>
-      </CardHeader>
       <Collapsible open={isOpen} onOpenChange={() => onToggle()}>
-        <CardContent>
+        <CardHeader>
           <div className="flex items-center justify-between p-3 border-b border-border">
-            <div className="flex items-center gap-2">{id}</div>
+            <div className="flex items-center gap-2">
+              <Button
+                ref={handleRef}
+                variant="ghost"
+                size="icon"
+                className="cursor-grab opacity-60 hover:opacity-100 flex gap-2 items-center"
+              >
+                <GripVertical className="size-4" />
+              </Button>
+
+              <CardTitle>
+                <p className="text-md font-semibold capitalize">{title}</p>
+              </CardTitle>
+
+              <CollapsibleTrigger>
+                <Button variant="ghost" size="icon">
+                  {isOpen ? (
+                    <ChevronDown className="size-4" />
+                  ) : (
+                    <ChevronRight className="size-4" />
+                  )}
+                </Button>
+              </CollapsibleTrigger>
+            </div>
           </div>
-        </CardContent>
+        </CardHeader>
+
+        <CollapsibleContent>
+          <div className="p-1"></div>
+        </CollapsibleContent>
       </Collapsible>
     </Card>
   );
@@ -64,9 +88,7 @@ export function CourseStructure({ courseData }: Props) {
   const toggleChapter = ({ chapterId }: { chapterId: string }) => {
     setInitialItems(() =>
       initialItems.map((chapter) =>
-        chapter.id === chapterId
-          ? { ...chapter, isOpen: !chapter.isOpen }
-          : chapter,
+        chapter.id === chapterId ? { ...chapter, isOpen: !chapter.isOpen } : chapter,
       ),
     );
   };
@@ -99,7 +121,7 @@ export function CourseStructure({ courseData }: Props) {
         <CardContent>
           <ul className="list">
             {initialItems.map((chapter) => (
-              <Sortable
+              <ChapterSortable
                 key={chapter.id}
                 id={chapter.id}
                 index={chapter.order}
